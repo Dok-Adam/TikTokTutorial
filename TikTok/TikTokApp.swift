fdbce57e7f546b9d7f024371d1576fcf2a0e9735
Adam <doktoradam72@gmail.com>
2023-11-18 19:26:38 +0500

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Адам Дотмерзаев on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
